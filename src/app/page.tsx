20db785a3e2dc9104508b160645202e1bad909a8
Marlon Angeli <iam@marlonangeli.com.br>
2024-01-15 14:31:41 -0300

    update structure and increment terminal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { TerminalComponent } from "@/components/terminal";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="container mx-auto">
-        <TerminalComponent />
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <nav className="flex items-center justify-between w-full">
+        <Link href="/" passHref>
+          <span className="ml-2 text-xl font-bold">Marlon Angeli</span>
+        </Link>
+        <Button type="button" variant="destructive">
+          Contato
+        </Button>
+        <ModeToggle />
+      </nav>
+
+      <div className="container mx-auto flex items-center">
+        <div className="w-2/3 pr-8">
+          <TerminalComponent />
+        </div>
+        <div className="w-1/3 ">
+          <h1 className="">Bem vindo ao meu portfólio!</h1>
+          <p>Parágrafo</p>
+          <Button type="button" variant="default">
+            Botão
+          </Button>
+        </div>
       </div>
     </main>
   );
--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -1,31 +1,109 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Terminal, {
   ColorMode,
   TerminalInput,
   TerminalOutput,
 } from "react-terminal-ui";
+import { createUniqueKey as key } from "@/lib/utils";
 
 export const TerminalComponent = () => {
+  const prompt = "user@portfolio:~$";
+  const [output, setOutput] = useState([
+    <TerminalOutput key={key()}>Olá, eu sou</TerminalOutput>,
+    <TerminalOutput key={key()}>{AnsiName}</TerminalOutput>,
+    <TerminalOutput key={key()}>
+      um desenvolvedor de software Back-end 🚀
+    </TerminalOutput>,
+    <TerminalOutput key={key()}></TerminalOutput>,
+    <TerminalOutput key={key()}>
+      Bem vindo ao meu portfólio! Digite &apos;help&apos; para ver os comandos
+      disponíveis
+    </TerminalOutput>,
+  ]);
+  const [errorCount, setErrorCount] = useState(0);
+
+  const handleInput = (input: string) => {
+    const command = [
+      <TerminalInput key={key()} prompt={prompt}>
+        {input}
+      </TerminalInput>,
+    ];
+
+    const commandMap = {
+      help: "exibe os comandos disponíveis",
+      clear: "limpa a tela do terminal",
+      skills: "mostra as minhas habilidades",
+      projects: "mostra os meus projetos",
+      contact: "mostra as minhas redes sociais",
+    };
+
+    let errorMessage = null;
+    const maxErrorCount = 2;
+    const incrementErrorCount = () =>
+      setErrorCount(errorCount < maxErrorCount ? errorCount + 1 : 0);
+
+    switch (input) {
+      case "help":
+        setOutput([
+          ...output,
+          ...command,
+          ...Object.entries(commandMap).map(([cmd, description]) => (
+            <TerminalOutput
+              key={key()}
+            >{`${cmd} - ${description}`}</TerminalOutput>
+          )),
+        ]);
+        break;
+      case "clear":
+        setOutput([]);
+        break;
+      case "":
+        setOutput([...output, ...command]);
+        break;
+      default:
+        errorMessage =
+          errorCount == maxErrorCount
+            ? `Digite 'help' para ver os comandos disponíveis`
+            : null;
+        incrementErrorCount();
+        setOutput([
+          ...output,
+          ...command,
+          <div className="text-red-600" key={key()}>
+            <TerminalOutput>
+              <span>{`Comando '${input}' não encontrado`}</span>
+            </TerminalOutput>
+            {errorMessage && <TerminalOutput>{errorMessage}</TerminalOutput>}
+          </div>,
+        ]);
+    }
+  };
+
   return (
-    <Terminal name="Terminal" colorMode={ColorMode.Dark}>
-      <p className="text-green-600">
-        <TerminalInput>cat saudação.txt</TerminalInput>
-      </p>
+    <Terminal
+      startingInputValue="help"
+      prompt={prompt}
+      name="Terminal in @portfolio"
+      onInput={handleInput}
+      colorMode={ColorMode.Dark}
+    >
       <TerminalOutput>
-        <p>
-          Bem vindo(a) ao meu portfolio!
-          <br />
-          <br />
-          Eu sou <strong>Marlon Angeli</strong>,
-          <br />
-          Um Desenvolvedor de Software Backend
-          <br />
-          <br />
-          Logo mais o site estará pronto!
-        </p>
+        <div className="text-wrap">{output}</div>
       </TerminalOutput>
     </Terminal>
   );
 };
+
+const AnsiName = `
+  __  __            _                                         _ _ 
+ |  \\/  |          | |                 /\\                    | (_)
+ | \\  / | __ _ _ __| | ___  _ __      /  \\   _ __   __ _  ___| |_ 
+ | |\\/| |/ _\\ | '__| |/ _ \\| '_ \\    / /\\ \\ | '_ \\ / _\\ |/ _ \\ | |
+ | |  | | (_| | |  | | (_) | | | |  / ____ \\| | | | (_| |  __/ | |
+ |_|  |_|\\__,_|_|  |_|\\___/|_| |_| /_/    \\_\\_| |_|\\__, |\\___|_|_|
+                                                    __/ |         
+                                                   |___/          
+
+`;
