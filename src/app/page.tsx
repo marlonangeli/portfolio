7fac4ab908e6bb891be61f98ab6ce0dd9d5e0685
Marlon Angeli <iam@marlonangeli.com.br>
2024-01-15 15:12:02 -0300

    update tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { TerminalComponent } from "@/components/terminal";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <nav className="flex items-center justify-between w-full">
+    <main className="flex min-h-screen flex-col p-3 max-w-screen-xl mx-auto">
+      <nav className="flex items-center justify-between w-full mb-10">
         <Link href="/" passHref>
           <span className="ml-2 text-xl font-bold">Marlon Angeli</span>
         </Link>
@@ -17,18 +16,28 @@ export default function Home() {
         <ModeToggle />
       </nav>
 
-      <div className="container mx-auto flex items-center">
+      <section className="container mx-auto flex items-center">
         <div className="w-2/3 pr-8">
           <TerminalComponent />
         </div>
-        <div className="w-1/3 ">
-          <h1 className="">Bem vindo ao meu portfólio!</h1>
-          <p>Parágrafo</p>
-          <Button type="button" variant="default">
-            Botão
-          </Button>
+        <div className="w-1/3 text-center">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
+            Bem vindo ao meu portfólio! 🚀
+          </h1>
+          <p>
+            O terminal ao lado é interativo, você pode navegar e brincar a
+            vontade, será que consegue encontrar meu segredo?
+          </p>
+          <div className="mt-10 flex justify-center flex-wrap space-x-4">
+            <Button type="button" variant="default">
+              Contato
+            </Button>
+            <Button type="button" variant="secondary">
+              Baixar CV
+            </Button>
+          </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
