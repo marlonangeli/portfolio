b770b0c8e1db94044dda23f56355b6ee9b0165fb
Marlon Angeli <iam@marlonangeli.com.br>
2024-01-29 22:02:07 -0300

    add zod validation

--- src/lib/duolingo.ts
+++ src/lib/duolingo.ts
@@ -1,23 +1,29 @@
-const API_URL =
+import { z } from "zod";
+
+const DUOLINGO_API_URL =
   "https://duolingo-tracker.marlonangeli.com.br/api/stats/marlonangeli";
 
-export type DuolingoCourse = {
-  title: string;
-  xp: number;
-};
+export const DuolingoCourseSchema = z.object({
+  title: z.string(),
+  xp: z.number(),
+});
 
-export type DuolingoUser = {
-  username: string;
-  streak: number;
-  courses: DuolingoCourse[];
-  id: string;
-  name: string;
-  picture: string;
-  totalXp: number;
-};
+export const DuolingoUserSchema = z.object({
+  username: z.string(),
+  streak: z.number(),
+  courses: z.array(DuolingoCourseSchema),
+  id: z.string(),
+  name: z.string(),
+  picture: z.string(),
+  totalXp: z.number(),
+});
+
+export type DuolingoCourse = z.infer<typeof DuolingoCourseSchema>;
+
+export type DuolingoUser = z.infer<typeof DuolingoUserSchema>;
 
 export async function getDuolingoData(): Promise<DuolingoUser> {
-  const response = await fetch(API_URL, {
+  const response = await fetch(DUOLINGO_API_URL, {
     headers: {
       "Content-Type": "application/json",
     },
